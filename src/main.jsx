a630faa050ba3b8b1f871715e12e08de6413f8a4
Joel <joel.thayu@student.moringaschool.com>
2025-04-14 15:32:24 +0300

    i am almost done

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ExpenseForm from './assets/componet/ExpenseForm.jsx'
+import ExpenseTable from './assets/componet/ExpenseTable.jsx'
+import SearchBar from './assets/componet/ SearchBar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ExpenseForm/>
+    <ExpenseTable/>
+    <SearchBar/>
   </StrictMode>,
 )
